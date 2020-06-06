90e553f4cfb1f387a4c2d1c76a59e42f80167001
hanskristian7 <hans.kristian1509@gmail.com>
2020-06-07 01:04:13 +0700

    add ajax xmlhttprequest api

---
+++ app/controllers/stocks_controller.rb
@@ -0,0 +1,25 @@
+class StocksController < ApplicationController
+    def search
+        # byebug
+        
+        if params[:stock].present?
+            @stock = Stock.new_lookup(params[:stock])
+            if @stock
+                respond_to do |format|
+                    # expecting partial js file
+                    format.js { render partial: 'users/result' }
+                end
+            else
+                respond_to do |format|
+                    flash.now[:alert] = "Please enter a valid symbol"
+                    format.js { render partial: 'users/result' }
+                end
+            end 
+        else
+            respond_to do |format|
+                flash.now[:alert] = "Please enter symbol to search"
+                format.js { render partial: 'users/result' }
+            end
+        end
+    end
+end
