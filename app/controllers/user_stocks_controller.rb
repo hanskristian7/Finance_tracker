71e7ac82652a7946c6eeb3a04c4f22edaa48d885
hanskristian7 <hans.kristian1509@gmail.com>
2020-06-07 14:52:09 +0700

    add name on navbar

--- app/controllers/user_stocks_controller.rb
+++ app/controllers/user_stocks_controller.rb
@@ -1,3 +1,21 @@
 class UserStocksController < ApplicationController
-    
+    def create
+        stock = Stock.check_db(params[:ticker])
+
+        if stock.blank?
+            stock = Stock.new_lookup(params[:ticker])
+            stock.save            
+        end
+        @user_stock = UserStock.create(user: current_user, stock: stock)
+        flash[:notice] = "stock #{stock.name} was successfully added!"
+        redirect_to myportfolio_path
+    end
+
+    def destroy
+        stock = Stock.find(params[:id])
+        user_stock = UserStock.where(user_id: current_user.id, stock_id: stock.id).first
+        user_stock.destroy
+        flash[:notice] = "#{stock.ticker} was successfully removed!"
+        redirect_to myportfolio_path
+    end
 end
